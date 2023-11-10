ddcb5b169d10e9edb4d62087897fe3c62611bf0c
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-11-10 22:55:48 +0100

    New scores

--- tools/benchmark_slurm.py
+++ tools/benchmark_slurm.py
@@ -14,6 +14,6 @@ for param_values in product(*param_grid.values()):
     params = dict(zip(param_names, param_values))
 
     const = "--job-name=benchmark --nodes=1 --time=02:00:00 -p gpua100 --gres=gpu:1 --cpus-per-task=4"
-    pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation; cd llm-distillation"
-    command = f"sbatch {const} --wrap=\"{pre_script} python benchmark/benchmark.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']}\""
+    pre_script = "cd /gpfs/users/boizardni/llm-distillation/benchmark/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
+    command = f"sbatch {const} --wrap=\"{pre_script} python benchmark.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']}\""
     subprocess.call(command ,shell=True)
--- tools/train_fsdp_slurm.py
+++ tools/train_fsdp_slurm.py
@@ -25,7 +25,7 @@ for param_values in product(*param_grid.values()):
 
     subprocess.call(f"mkdir {output_path}", shell=True)
 
-    const = "--job-name=FSDP --nodes=1 --time=1:00:00 -p gpua100 --gres=gpu:4 --cpus-per-task=10 --mem-per-cpu=12G"
+    const = "--job-name=FSDP --nodes=1 --time=24:00:00 -p gpua100 --gres=gpu:4 --cpus-per-task=10 --mem-per-cpu=12G"
     pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
     command = f"sbatch {const} --wrap=\"{pre_script} torchrun --nnodes 1 --nproc_per_node 4 llm-recipes/finetuning.py --project_name llm-distillation --enable_fsdp --model_name {params['model_name']} --dist_checkpoint_root_folder {output_path} --custom_dataset.file {params['dataset']} --lr {params['lr']} --num_epochs {params['num_epochs']} --batch_size_training {params['batch_size_training']} --val_batch_size {params['val_batch_size']} --weight_decay {params['weight_decay']} --final_div_factor {params['final_div_factor']} --pct_start {params['pct_start']} --fsdp_config.pure_bf16 --use_fast_kernels\""
     subprocess.call(command ,shell=True)
