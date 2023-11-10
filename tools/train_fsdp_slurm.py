f37fd436cc481f016a0975aa1363cdd296bde1ed
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-11-10 14:31:32 +0100

    FSDP fast kernel a100

--- tools/train_fsdp_slurm.py
+++ tools/train_fsdp_slurm.py
@@ -9,7 +9,7 @@ param_grid = {
     'val_batch_size': [8],
     'weight_decay': [0.1],
     'final_div_factor': [5],
-    'pct_start': [0.01],
+    'pct_start': [0.05],
 
     'dataset': [
         '/gpfs/users/boizardni/llm-distillation/datasets/loader/squad.py'
@@ -25,7 +25,7 @@ for param_values in product(*param_grid.values()):
 
     subprocess.call(f"mkdir {output_path}", shell=True)
 
-    const = "--job-name=FSDP --nodes=1 --time=00:30:00 -p gpu_test --gres=gpu:4 --cpus-per-task=10 --mem-per-cpu=12G"
-    pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation; export OMP_NUM_THREADS=2;"
-    command = f"sbatch {const} --wrap=\"{pre_script} torchrun --nnodes 1 --nproc_per_node 4 llm-recipes/finetuning.py --project_name llm-distillation --enable_fsdp --model_name {params['model_name']} --dist_checkpoint_root_folder {output_path} --custom_dataset.file {params['dataset']} --lr {params['lr']} --num_epochs {params['num_epochs']} --batch_size_training {params['batch_size_training']} --val_batch_size {params['val_batch_size']} --weight_decay {params['weight_decay']} --final_div_factor {params['final_div_factor']} --pct_start {params['pct_start']} --fsdp_config.pure_bf16 ----use_fast_kernels\""
+    const = "--job-name=FSDP --nodes=1 --time=1:00:00 -p gpua100 --gres=gpu:4 --cpus-per-task=10 --mem-per-cpu=12G"
+    pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
+    command = f"sbatch {const} --wrap=\"{pre_script} torchrun --nnodes 1 --nproc_per_node 4 llm-recipes/finetuning.py --project_name llm-distillation --enable_fsdp --model_name {params['model_name']} --dist_checkpoint_root_folder {output_path} --custom_dataset.file {params['dataset']} --lr {params['lr']} --num_epochs {params['num_epochs']} --batch_size_training {params['batch_size_training']} --val_batch_size {params['val_batch_size']} --weight_decay {params['weight_decay']} --final_div_factor {params['final_div_factor']} --pct_start {params['pct_start']} --fsdp_config.pure_bf16 --use_fast_kernels\""
     subprocess.call(command ,shell=True)
