0ca08836d61a48527d6caf3cc36219a0e8619993
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-12-12 00:01:52 +0100

    Fix path save dataset

--- datasets/generator.py
+++ datasets/generator.py
@@ -108,5 +108,5 @@ if __name__ == "__main__":
         'answers': [item['text'] for item in dataset['answers']],
         'answers_generated': list(chain(*predictions))
     })
-    dataset_generated.save_to_disk(f'/gpfs/users/boizardni/llm-distillation/datasets/generated/{args.model_id.split("/")[-1]}_{args.dataset_id}_{args.split_name}')
+    dataset_generated.save_to_disk(f'generated/{args.model_id.split("/")[-1]}_{args.dataset_id}_{args.split_name}')
     logging.info('Dataset saved')
