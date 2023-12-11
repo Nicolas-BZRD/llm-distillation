2a72d8456d8bacf50a93ffce4ef6cf9935cd86ca
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-12-11 15:10:48 +0100

    Ikitracs dataset

--- jobs/benchmark_slurm.py
+++ jobs/benchmark_slurm.py
@@ -2,11 +2,11 @@ import subprocess
 from itertools import product
 
 param_grid = {
-    'model_id': ["EleutherAI/pythia-410m-deduped"],
-    'model_tokenizer': ["EleutherAI/pythia-410m-deduped"],
-    'dataset_id': ['adversarial_qa'],
-    'split_name': ['test', 'validation'],
-    'number_few_shot': [5],
+    'model_id': ["meta-llama/Llama-2-7b-hf"],
+    'model_tokenizer': ["meta-llama/Llama-2-7b-hf"],
+    'dataset_id': ['/gpfs/users/boizardni/llm-distillation/datasets/generated/ikitracs-qa/test'],
+    'split_name': ['train'],
+    'number_few_shot': [0, 2, 5],
 }
 param_names = param_grid.keys()
 
@@ -15,5 +15,5 @@ for param_values in product(*param_grid.values()):
 
     const = "--job-name=benchmark --nodes=1 --time=00:10:00 -p gpu_test --gres=gpu:1 --cpus-per-task=8 --mem-per-cpu=32G"
     pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
-    command = f"sbatch {const} --wrap=\"{pre_script} python llm-distillation/benchmark/benchmark.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --context --subset_name adversarialQA\""
+    command = f"sbatch {const} --wrap=\"{pre_script} python llm-distillation/benchmark/benchmark.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --context\""
     subprocess.call(command ,shell=True)
--- jobs/generator_slurm.py
+++ jobs/generator_slurm.py
@@ -4,8 +4,11 @@ from itertools import product
 param_grid = {
     'model_id': ['meta-llama/Llama-2-7b-hf',],
     'model_tokenizer': ["meta-llama/Llama-2-7b-hf"],
-    'dataset_id': ['adversarial_qa'],
-    'split_name': ['train', 'validation'],
+    'dataset_id': [
+        '/gpfs/users/boizardni/llm-distillation/datasets/generated/ikitracs-qa/train',
+        # '/gpfs/users/boizardni/llm-distillation/datasets/generated/ikitracs-qa/validation'
+        ],
+    'split_name': ['train'],
     'number_few_shot': [2],
 }
 param_names = param_grid.keys()
@@ -15,5 +18,5 @@ for param_values in product(*param_grid.values()):
 
     const = "--job-name=generator --nodes=1 --time=02:00:00 -p gpua100 --gres=gpu:1 --cpus-per-task=8 --mem-per-cpu=32G"
     pre_script = "cd /gpfs/users/boizardni/llm-distillation; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
-    command = f"sbatch {const} --wrap=\"{pre_script} python datasets/generator.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --subset_name adversarialQA --context --bfloat\""
+    command = f"sbatch {const} --wrap=\"{pre_script} python datasets/generator.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --context --bfloat\""
     subprocess.call(command ,shell=True)
