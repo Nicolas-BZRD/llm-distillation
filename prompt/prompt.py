005394238e60a0058864ceb06cc6dbb79324b029
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-12-22 18:52:10 +0100

    Remove unused files and update job configurations

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -12,6 +12,23 @@ def __load_module_from_py_file(py_file: str) -> object:
     loader.exec_module(module)
     return module
 
+def create_prompt(task: str, few_shot: int, *args):
+    prompt = ""
+    if args[0].get('context', False):
+        with open(f"{os.getenv('HOME')}/llm-distillation/prompt/context.json") as json_file:
+            prompt += json.load(json_file)[task]+"\n\n"
+
+    module = __load_module_from_py_file(f"{os.getenv('HOME')}/llm-distillation/prompt/few_shot/{task}.py")
+    request = getattr(module, "create_request")(**args[0])
+
+    if few_shot:
+        module = __load_module_from_py_file(f"{os.getenv('HOME')}/llm-distillation/prompt/few_shot/{task}.py")
+        shot = getattr(module, "create_few_shot")(few_shot, args[0])
+        prompt += ('\n\n').join(shot) + f"\n\n{request}"
+    else:
+        prompt += request
+    return prompt   
+
 def llama_chat_prompt(task: str, few_shot: int, *args):
     prompt = ""
     with open(f"{os.getenv('HOME')}/llm-distillation/prompt/context.json") as json_file:
