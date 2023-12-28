648194d777819f93dd65d1998bc8a0cd4a5756f4
Nicolas-BZRD <nicolas.bzrd@gmail.com>
2023-12-28 17:56:55 +0100

    New datasets (squad, dialogsum) with one loader (squad)

--- jobs/distillation/distillation_fsdp_slurm.py
+++ jobs/distillation/distillation_fsdp_slurm.py
@@ -7,12 +7,12 @@ param_grid = {
     'lr': [1e-6],
     'num_epochs': [5],
     'batch_size_training': [2],
-    'val_batch_size': [4],
+    'val_batch_size': [2],
     'final_div_factor': [5],
-    'distil_factor': [1],
+    'distil_factor': [0.2],
     'temperature': [1],
 
-    'dataset': ['/gpfs/users/boizardni/llm-distillation/datasets/loader/fairytaleQA_llama.py']
+    'dataset': ['/gpfs/users/boizardni/llm-distillation/datasets/loader/squad_llama.py']
 }
 param_names = param_grid.keys()
 
@@ -24,7 +24,7 @@ for param_values in product(*param_grid.values()):
 
     subprocess.call(f"mkdir {output_path}", shell=True)
 
-    const = "--job-name=DIST_FSDP --nodes=1 --time=00:30:00 -p gpua100 --gres=gpu:2 --cpus-per-task=10 --mem-per-cpu=32G"
+    const = "--job-name=DIST_FSDP --nodes=1 --time=01:00:00 -p gpu_test --gres=gpu:4 --cpus-per-task=10 --mem-per-cpu=32G"
     pre_script = "cd /gpfs/users/boizardni/; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
-    command = f"sbatch {const} --wrap=\"{pre_script} torchrun --nnodes 1 --nproc_per_node 2 --master_port 29501 llm-recipes/finetuning.py --project_name test --model_name {params['model_name']} --dataset.file {params['dataset']} --lr {params['lr']} --num_epochs {params['num_epochs']} --batch_size_training {params['batch_size_training']} --val_batch_size {params['val_batch_size']} --final_div_factor {params['final_div_factor']} --output_dir {output_path} --distillation_config.model_name {params['teacher_model_name']} --distillation --distillation_config.enable_fsdp --distillation_config.pure_bf16 --distillation_config.distil_factor {params['distil_factor']} --distillation_config.temperature {params['temperature']} --save_step 300 --seed 4\""
+    command = f"sbatch {const} --wrap=\"{pre_script} torchrun --nnodes 1 --nproc_per_node 4 --master_port 29501 llm-recipes/finetuning.py --project_name test --model_name {params['model_name']} --dataset.file {params['dataset']} --lr {params['lr']} --num_epochs {params['num_epochs']} --batch_size_training {params['batch_size_training']} --val_batch_size {params['val_batch_size']} --final_div_factor {params['final_div_factor']} --output_dir {output_path} --distillation_config.model_name {params['teacher_model_name']} --distillation --distillation_config.enable_fsdp --distillation_config.pure_bf16 --distillation_config.distil_factor {params['distil_factor']} --distillation_config.temperature {params['temperature']} --save_step 300 --seed 4\""
     subprocess.call(command ,shell=True)
--- jobs/generator_slurm.py
+++ jobs/generator_slurm.py
@@ -4,9 +4,9 @@ from itertools import product
 param_grid = {
     'model_id': ['meta-llama/Llama-2-7b-chat-hf',],
     'model_tokenizer': ["meta-llama/Llama-2-7b-chat-hf"],
-    'dataset_id': ['GEM/FairytaleQA'],
-    'split_name': ['validation'],
-    'number_few_shot': [4],
+    'dataset_id': ['knkarthick/dialogsum'],
+    'split_name': ['train'],
+    'number_few_shot': [3],
     'batch_size': [2],
 }
 param_names = param_grid.keys()
@@ -14,7 +14,7 @@ param_names = param_grid.keys()
 for param_values in product(*param_grid.values()):
     params = dict(zip(param_names, param_values))
 
-    const = "--job-name=generator --nodes=1 --time=01:00:00 -p gpua100 --gres=gpu:1 --cpus-per-task=8 --mem-per-cpu=32G"
+    const = "--job-name=generator --nodes=1 --time=05:00:00 -p gpua100 --gres=gpu:1 --cpus-per-task=8 --mem-per-cpu=32G"
     pre_script = "cd /gpfs/users/boizardni/llm-distillation; module load anaconda3/2020.02/gcc-9.2.0; source activate llm_distillation;"
-    command = f"sbatch {const} --wrap=\"{pre_script} python datasets/generator.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --batch_size {params['batch_size']} --context --bfloat --task qa_generative --mapping /gpfs/users/boizardni/llm-distillation/benchmark/mapping/FairytaleQA.json\""
+    command = f"sbatch {const} --wrap=\"{pre_script} python datasets/generator.py --model_id {params['model_id']} --model_tokenizer {params['model_tokenizer']} --dataset_id {params['dataset_id']} --split_name {params['split_name']} --number_few_shot {params['number_few_shot']} --batch_size {params['batch_size']} --context --bfloat --task summary_dialogue --mapping /gpfs/users/boizardni/llm-distillation/benchmark/mapping/dialogsum.json\""
     subprocess.call(command ,shell=True)
